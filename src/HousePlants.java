b8a6aa1b6d15a6744d0b6ca6efeb91cfb821f735
VaclavKuchar1972 <vkcomp@vkcomp.cz>
2023-07-06 13:34:20 +0200

    DoOprava domácího úkolu lekce č.5 - přesunutí ošetření opakujícího se kódu ze třídy Setting do třídy HousePlants.

--- src/HousePlants.java
+++ src/HousePlants.java
@@ -4,14 +4,32 @@ import com.domaciukollekce6.houseplants.PlantManager;
 import com.domaciukollekce6.houseplants.Settings;
 
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
+import static com.domaciukollekce6.houseplants.Settings.delimiter;
+
 public class HousePlants {
+
+    // I když jsi mi psal, že to mám dát do třídy Plant, napsal jsi mi i přesně to jak to beru, ano jsou to z mého
+    // pohledu čistě pomocné metody pro třídu HousePlants, čistě z toho důvodu, aby se mi neopakoval stejný kód na více
+    // místech, nic víc, žádná atomová věda. :-) Děkuji. V žádných jiných třídách tyto metody nejsou a nebudou potřeba.
+    private static void printPlantsPeopleDateOutput(Plant plant) {
+        System.out.println(delimiter() + plant.getPlantName() + delimiter() + plant.getPlantNote() + delimiter()
+                + plant.getPlantNormalWateringFrequency() + delimiter()
+                + plant.getPlantLastWateringDate().format(DateTimeFormatter.ofPattern("d.M.yyyy")) + delimiter()
+                + plant.getPlantPlantingDate().format(DateTimeFormatter.ofPattern("d.M.yyyy")));
+    }
+    private static void printPlantsComputerOutput(Plant plant) {
+        System.out.println(delimiter() + plant.getPlantName() + delimiter() + plant.getPlantNote() + delimiter()
+                + plant.getPlantNormalWateringFrequency() + delimiter() + plant.getPlantLastWateringDate()
+                + delimiter() + plant.getPlantPlantingDate());
+    }
     public static void main(String[] args) throws PlantException {
 
         PlantManager plantManager = new PlantManager();
 
-        try {plantManager.loadDataPlantsFromFile(Settings.fileNamePrimary(), Settings.delimiter());}
+        try {plantManager.loadDataPlantsFromFile(Settings.fileNamePrimary(), delimiter());}
         catch (PlantException e) {
             System.err.println("Nepodařilo se načíst data ze souboru " + e.getLocalizedMessage());
             throw new RuntimeException(e);
@@ -19,43 +37,41 @@ public class HousePlants {
 
         // Toto musí být až po natažení dat, jinak je list prázdný,
         // narozdíl od ostatních tříd se Main evidentně chová striktně chronologicky!!! (to je pro mě)
-        List<Plant> plantListPM = plantManager.getPlantList();
+        List<Plant> plantList = plantManager.getPlantList();
 
         System.out.println();
         System.out.println("Surová data ze VSTUPNÍHO souboru DB-ListOfPlantsPrimary.txt:");
-        for (Plant plant : plantListPM) {Settings.printPlantsComputerOutput(plant);}
+        for (Plant plant : plantList) {printPlantsComputerOutput(plant);}
 
         System.out.println();
         System.out.println("Informace o zálivce dle zadání domácího úkolu v bodě 13:");
-        for (Plant plant : plantListPM) {Settings.printPlantsComputerOutput(plant);}
+        for (Plant plant : plantList) {printPlantsComputerOutput(plant);}
 
 
         try {
-            plantListPM.add(new Plant("Jahodník", "na zábradlí balkónu", LocalDate.now(),
+            plantList.add(new Plant("Jahodník", "na zábradlí balkónu", LocalDate.now(),
                     LocalDate.now(),
                     3));
-            plantListPM.add(new Plant("Mochíto Máta", "na balkóně", LocalDate.now(),
+            plantList.add(new Plant("Mochíto Máta", "na balkóně", LocalDate.now(),
                     LocalDate.now(), 2));
         }
         catch (PlantException e) {
             System.err.print("Nastala chyba při vytváření nových rostlin" + e.getLocalizedMessage());
         }
 
-
         System.out.println();
         System.out.println("Aktualizovaný seznam rostlin po přidání dvou rostlin dle zadání domácího úkolu v bodě 14:");
-        for (Plant plant : plantListPM) {Settings.printPlantsPeopleDateOutput(plant);}
+        for (Plant plant : plantList) {printPlantsPeopleDateOutput(plant);}
 
-        plantListPM.removeIf(plant -> plant.getPlantName().equals("Sukulent v koupelně"));
+        plantList.removeIf(plant -> plant.getPlantName().equals("Sukulent v koupelně"));
         System.out.println();
         System.out.println
                 ("Aktualizovaný seznam rostlin po odebrání jedné rostliny dle zadání domácího úkolu v bodě 14:");
-        for (Plant plant : plantListPM) {Settings.printPlantsPeopleDateOutput(plant);}
-
+        for (Plant plant : plantList) {printPlantsPeopleDateOutput(plant);}
 
         System.out.println();
         try {
-            plantManager.saveDataPlantsToNewFile(Settings.fileNameAfterChanges(), plantListPM);
+            plantManager.saveDataPlantsToNewFile(Settings.fileNameAfterChanges(), plantList);
             System.out.println("Aktualizovaný seznam rostlin byl uložen do souboru: "
                     + Settings.fileNameAfterChanges());
         } catch (PlantException e) {
@@ -63,15 +79,14 @@ public class HousePlants {
             throw new RuntimeException(e);
         }
 
-
-        try {plantManager.loadDataPlantsFromFile(Settings.fileNameAfterChanges(), Settings.delimiter());}
+        try {plantManager.loadDataPlantsFromFile(Settings.fileNameAfterChanges(), delimiter());}
         catch (PlantException e) {
             System.err.println("Nepodařilo se načíst data ze souboru " + e.getLocalizedMessage());
             throw new RuntimeException(e);
         }
         System.out.println();
         System.out.println("Surová data z VÝSTUPNÍHO souboru DB-ListOfPlantsAfterChanges.txt:");
-        for (Plant plant : plantListPM) {Settings.printPlantsComputerOutput(plant);}
+        for (Plant plant : plantList) {printPlantsComputerOutput(plant);}
 
     }
 
--- src/com/domaciukollekce6/houseplants/Settings.java
+++ src/com/domaciukollekce6/houseplants/Settings.java
@@ -7,17 +7,4 @@ public class Settings {
     public static String fileNameAfterChanges() {return "DB-ListOfPlantsAfterChanges.txt";}
     public static String delimiter() {return "\t";}
 
-    public static void printPlantsPeopleDateOutput(Plant plant) {
-        System.out.println("\t" + plant.getPlantName() + delimiter() + plant.getPlantNote() + delimiter()
-                + plant.getPlantNormalWateringFrequency() + delimiter()
-                + plant.getPlantLastWateringDate().format(DateTimeFormatter.ofPattern("d.M.yyyy")) + delimiter()
-                + plant.getPlantPlantingDate().format(DateTimeFormatter.ofPattern("d.M.yyyy")));
-    }
-
-    public static void printPlantsComputerOutput(Plant plant) {
-        System.out.println("\t" + plant.getPlantName() + delimiter() + plant.getPlantNote() + delimiter()
-                + plant.getPlantNormalWateringFrequency() + delimiter() + plant.getPlantLastWateringDate()
-                + delimiter() + plant.getPlantPlantingDate());
-    }
-
 }
